53372e2c55ae470bd45f7ebfe65acca0a01a2b90
Rafael Ernesto Ferro <rafix81@gmail.com>
2011-09-06 09:28:58 -0400

    Added DmTagPlugin

---
+++ lib/form/doctrine/dmTagPlugin/DmTagForm.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * DmTag form.
+ *
+ * @package    diem-dealr
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ * @generator  Diem 5.4.0-DEV
+ */
+class DmTagForm extends PluginDmTagForm
+{
+  public function configure()
+  {
+    parent::configure();
+  }
+}
