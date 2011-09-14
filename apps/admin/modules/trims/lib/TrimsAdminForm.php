9be7015d00aa119ec3cdb72eca7e195f94b13520
Rafael Ernesto Ferro <rafix81@gmail.com>
2011-09-14 16:49:55 -0400

    Added Dependent Select to Trims Form

--- apps/admin/modules/trims/lib/TrimsAdminForm.php
+++ apps/admin/modules/trims/lib/TrimsAdminForm.php
@@ -12,5 +12,31 @@ class TrimsAdminForm extends BaseTrimsForm
   public function configure()
   {
     parent::configure();
+    
+    $this->widgetSchema['make_id'] = new sfWidgetFormDoctrineDependentSelect(array(
+        'model' => 'Make',
+        'add_empty' => 'Select one Make',
+        'ajax'      => true,
+        'cache' => true,
+    ));
+    
+    $this->widgetSchema['model_id'] = new sfWidgetFormDoctrineDependentSelect(array(
+        'model' => 'Model',
+        'depends' => 'Make',
+        'add_empty' => 'Select one Model',
+        'ajax'      => true,
+        'cache' => true,
+    ));
+    
+    $this->widgetSchema->moveField('model_id', 'after', 'make_id');
+    
+    $this->validatorSchema['make_id'] = new sfValidatorDoctrineChoice(array(
+        'model' => 'Make',
+    ));
+    
+    $this->validatorSchema['model_id'] = new sfValidatorDoctrineChoice(array(
+        'model' => 'Model',
+    ));
+    
   }
 }
